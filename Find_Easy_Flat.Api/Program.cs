09c6daf622a623325e7ce1f72b6b658af865e21f
Jasurbek Mamatqulov <mamatqulovjasurbek805@outlook.com>
2023-01-18 18:39:02 +0500

    CONFIC:Register Broker

--- Find_Easy_Flat.Api/Program.cs
+++ Find_Easy_Flat.Api/Program.cs
@@ -1,3 +1,5 @@
+using Find_Easy_Flat.Api.Brokers.StorageBroker;
+
 namespace Find_Easy_Flat.Api
 {
     public class Program
@@ -8,9 +10,9 @@ namespace Find_Easy_Flat.Api
 
             builder.Services.AddControllers();
 
+            builder.Services.AddTransient<IStorageBroker, StorageBroker>();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
             var app = builder.Build();
 
             if (app.Environment.IsDevelopment())
